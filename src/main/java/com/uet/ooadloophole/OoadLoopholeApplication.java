33114705617615fc28b0ccdebf962b7ebe5b4b4e
Nguyen Ngoc Lam <lamnn718@gmail.com>
2019-09-17 15:24:39 +0700

    init

---
+++ src/main/java/com/uet/ooadloophole/OoadLoopholeApplication.java
@@ -0,0 +1,13 @@
+package com.uet.ooadloophole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoadLoopholeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OoadLoopholeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uet/ooadloophole/OoadLoopholeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uet.ooadloophole;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OoadLoopholeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
