61eaedaa1f769b625c6d53e085f6853ee36a9f80
Nguyen Ngoc Lam <lamnn718@gmail.com>
2019-12-12 15:40:44 +0700

    re-do login page

--- src/main/java/com/uet/ooadloophole/config/WebSecurityConfig.java
+++ src/main/java/com/uet/ooadloophole/config/WebSecurityConfig.java
@@ -80,7 +80,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**",
-                        "/templates/**", "/css/libraries/**", "/js/libraries/**", "/fonts/**", "/fonts/fonts/**", "/fonts/less/**", "/fonts/scss/**");
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/js/libraries/**", "/font/**", "/font/roboto/**");
     }
 }
