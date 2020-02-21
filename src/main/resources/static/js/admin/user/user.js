0c1b6b0133e589cd7125d3580d73f5368c22aada
Phong Ha Tuan <phongakatoaa@gmail.com>
2020-02-21 13:37:28 +0700

    add new user management form

--- src/main/resources/static/js/admin/user/user.js
+++ src/main/resources/static/js/admin/user/user.js
@@ -39,7 +39,80 @@ var userTable = $(".teacher-admin .ui.table").DataTable({
         var actionCell = $(row).children().eq(1);
         var btnEdit = $('<button type="button" class="ui mini icon blue button"><i class="pencil icon"></i></button>')
             .click(function () {
+                $('.form.edit-user').form('set values', {
+                    id: data._id,
+                    fullName: data.fullName,
+                    username: data.username,
+                    email: data.email,
+                    isAdmin: userHasRole(data, 'admin'),
+                    isTeacher: userHasRole(data, 'teacher')
+                });
                 showModal('.modal.edit-user');
+            });
+        var btnDelete = $('<button type="button" class="ui mini icon grey button"><i class="trash icon"></i></button>')
+            .click(function () {
+                $('.form.delete-user').form('set values', {
+                    id: data._id
+                });
+                showModal('.modal.delete-user');
+            });
+        var btnDeactivate = $('<button type="button" class="ui mini icon red button"><i class="ban icon"></i></button>')
+            .click(function () {
+                $('.form.deactivate-user').form('set values', {
+                    id: data._id
+                });
+                showModal('.modal.deactivate-user');
+            });
+        actionCell.append(
+            btnEdit, $('<span>&nbsp</span>'),
+            btnDeactivate, $('<span>&nbsp</span>'),
+            btnDelete
+        );
+    }
+});
+
+rowIndex = 0;
+var studentTable = $(".student .ui.table").DataTable({
+    ordering: true,
+    searching: true,
+    paging: true,
+    autoWidth: true,
+    lengthChange: true,
+    ajax: "/api/students",
+    sDom: stringDom,
+    language: languageOption,
+    columns: [
+        {
+            data: null,
+            render: function () {
+                return ++rowIndex;
+            }
+        },
+        {
+            data: null,
+            render: function () {
+                return "";
+            }
+        },
+        {data: "fullName"},
+        {data: "studentId"},
+        {data: "className"},
+        {
+            data: "active",
+            render: function (isActive) {
+                if (isActive) return "<i class='green check icon'></i>";
+                else return "<i class='red ban icon'></i>"
+            }
+        }
+    ],
+    columnDefs: [
+        {targets: [0, 1, -1], className: "center aligned"}
+    ],
+    createdRow: function (row, data) {
+        var actionCell = $(row).children().eq(1);
+        var btnEdit = $('<button type="button" class="ui mini icon blue button"><i class="pencil icon"></i></button>')
+            .click(function () {
+                showModal('.modal.edit-student');
             });
         var btnDelete = $('<button type="button" class="ui mini icon grey button"><i class="trash icon"></i></button>')
             .click(function () {
@@ -56,12 +129,6 @@ var userTable = $(".teacher-admin .ui.table").DataTable({
         );
     }
 });
-
-$.fn.form.settings.rules.oneRoleClaimed = function () {
-    var isAdmin = $('#checkboxAdmin').prop('checked');
-    var isTeacher = $('#checkboxTeacher').prop('checked');
-    return isAdmin || isTeacher;
-};
 
 $('.form.create-user').form({
     onSuccess: function (evt, data) {
@@ -82,49 +149,58 @@ $('.form.create-user').form({
         });
     },
     fields: {
-        fullName: {
-            identifier: 'fullName',
-            rules: [
-                {
-                    type: 'regExp[/^[a-zA-Z\\-]+$/]',
-                    prompt: 'Họ tên không hợp lệ'
-                }
-            ]
-        },
-        username: {
-            identifier: 'username',
-            rules: [
-                {
-                    type: 'regExp[/^[a-zA-Z0-9]+$/]',
-                    prompt: 'Tên đăng nhập không hợp lệ'
-                }
-            ]
-        },
-        email: {
-            identifier: 'email',
-            rules: [
-                {
-                    type: 'email',
-                    prompt: 'Email không hợp lệ'
-                }
-            ]
-        },
-        oneRoleClaimed: {
-            identifier: 'role-checkbox',
-            rules: [{
-                type: 'oneRoleClaimed',
-                prompt: 'Vui lòng chọn ít nhất một quyền'
-            }]
-        }
+        fullName: validationRules.fullName,
+        username: validationRules.username,
+        email: validationRules.email,
+        oneRoleClaimed: validationRules.oneRoleClaimed('.form.create-user')
     }
 });
 
-$('#pageLengthSelect input').change(function () {
+$('.form.edit-user').form({
+    onSuccess: function (evt, data) {
+        showDimmer('.form.edit-user');
+        correctFormData('.form.edit-user', data);
+        $('.create-user .form').api({
+            action: 'update user',
+            urlData: {
+                id: data.id,
+            },
+            on: 'now',
+            method: 'put',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            onFailure: function (response) {
+                $('.edit.create-user').form('add errors', [response.message]);
+            },
+            onSuccess: function () {
+                userTable.ajax().reload();
+            }
+        });
+    },
+    fields: {
+        fullName: validationRules.fullName,
+        username: validationRules.username,
+        email: validationRules.email,
+        oneRoleClaimed: validationRules.oneRoleClaimed('.form.edit-user')
+    }
+});
+
+$('.form.delete-user').form();
+
+$('.teacher-admin input.page-length').change(function () {
     userTable.page.len(this.value).draw();
 });
 
-$('#searchTableInput input').keyup(function () {
+$('.teacher-admin input.table-search').keyup(function () {
     userTable.search(this.value).draw();
+});
+
+$('.student input.page-length').change(function () {
+    studentTable.page.len(this.value).draw();
+});
+
+$('.student input.table-search').keyup(function () {
+    studentTable.search(this.value).draw();
 });
 
 $('.item.teacher-admin').click(function () {
@@ -143,6 +219,13 @@ $('.item.student').click(function () {
 
 function filterRole(roleName) {
     rowIndex = 0;
-    userTable.ajax.url('/api/users/role/' + roleName).load();
+    userTable.ajax.url(`/api/users/role/${roleName}`).load();
     userTable.draw();
+}
+
+function userHasRole(user, roleName) {
+    for (var i = 0; i < user.roles.length; i++) {
+        if (user.roles[i].role.toLowerCase() === roleName.toLowerCase()) return true;
+    }
+    return false;
 }
