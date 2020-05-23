c8264b9d09fe01ac2f3b212980ab6303e54fd1b2
Lam Nguyen <lamnn718@gmail.com>
2020-05-23 17:23:13 +0700

    requirement view

--- src/main/java/com/uet/ooadloophole/database/grading_repositories/TemplateForPhaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uet.ooadloophole.database.grading_repositories;
-
-import com.uet.ooadloophole.model.business.grading_elements.TemplateForPhase;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TemplateForPhaseRepository extends MongoRepository<TemplateForPhase, String> {
-}
--- src/main/java/com/uet/ooadloophole/database/grading_repositories/TemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uet.ooadloophole.database.grading_repositories;
-
-import com.uet.ooadloophole.model.business.grading_elements.Template;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TemplateRepository extends MongoRepository<Template, String> {
-}
